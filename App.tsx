b8a19a2e7026d7c62970085bc88fb6ee7fda37ee
pivosoft <info@pivosoft.com>
2023-10-13 20:40:30 +0300

    setup startup components

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const Stack = createNativeStackNavigator()
 
 function App(): JSX.Element {
 
-  const [token, setToken] = useState('')
+  const [token, setToken] = useState('xxxx')
 
   return (
     <NavigationContainer>
