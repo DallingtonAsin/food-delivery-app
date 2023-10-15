e2992fd38e3ebae033174c5ec4a614ef8da52840
pivosoft <info@pivosoft.com>
2023-10-15 22:24:02 +0300

    completed delivery screen

--- app/src/screens/DeliveryScreen.tsx
+++ app/src/screens/DeliveryScreen.tsx
@@ -1,35 +1,84 @@
-import { Text, View } from "react-native"
+import { Image, Text, View } from "react-native"
 import { featured } from "../configs/data"
 import { useNavigation } from "@react-navigation/native"
 import { NativeStackNavigationProp } from '@react-navigation/native-stack'
 import MapView, { Marker } from "react-native-maps"
+import { themeColors } from "../configs/themes"
+import { TouchableOpacity } from "react-native-gesture-handler"
+import * as Icon from "react-native-feather"
+import { colors } from "../configs"
 
 const DeliveryScreen = () => {
-    const restaurant = featured.restaurants[0]
-    const navigation = useNavigation<NativeStackNavigationProp<any>>()
+  const restaurant = featured.restaurants[0]
+  const navigation = useNavigation<NativeStackNavigationProp<any>>()
 
-    return (
-        <View className="flex-1">
-             {/* map view  */}
-             <MapView
-              initialRegion={{
-                latitude: restaurant.lat,
-                longitude: restaurant.lng,
-                latitudeDelta: 0.01,
-                longitudeDelta: 0.01
-              }}
-              className="flex-1"
-              mapType="standard"
-             >
-                <Marker
-                  coordinate={{
-                    latitude: restaurant.lat,
-                    longitude: restaurant.lng
-                  }}
-                />
-             </MapView>
+  return (
+    <View className="flex-1">
+      {/* map view  */}
+      <MapView
+        initialRegion={{
+          latitude: restaurant.lat,
+          longitude: restaurant.lng,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01
+        }}
+        className="flex-1"
+        mapType="standard"
+      >
+        <Marker
+          coordinate={{
+            latitude: restaurant.lat,
+            longitude: restaurant.lng
+          }}
+          title={restaurant.name}
+          description={restaurant.description}
+          pinColor={themeColors.bgColor(1)}
+        />
+      </MapView>
+      <View className="rounded-t-3xl -mt-12 bg-white relative">
+        <View className="flex-row justify-between px-5 pt-10">
+          <View>
+            <Text className="text-lg text-gray-700 font-semibold">
+              Estimated Arrival
+            </Text>
+            <Text className="text-3xl font-extrabold text-gray-700">
+              20-30 Minutes
+            </Text>
+            <Text className="mt-2 font-semibold text-gray-700">
+              Your order is on its way!
+            </Text>
+          </View>
+          <Image className="w-24 h-24" source={require('../../assets/images/bikeGuy2.gif')} />
         </View>
-    )
+        <View
+          style={{ backgroundColor: themeColors.bgColor(0.8) }}
+          className="p-2 flex-row justify-between items-center rounded-full my-5 mx-2">
+          <View className="p-1 rounded-full"
+            style={{ backgroundColor: 'rgba(255,255,255,0.4' }}>
+            <Image className="h-16 w-16 rounded-full" source={require('../../assets/images/deliveryGuy.png')} />
+          </View>
+          <View className="flex-1 ml-3">
+            <Text className="text-lg font-bold text-white">
+              Dallington Azure
+            </Text>
+            <Text className="font-semibold text-white">
+              Your Rider
+            </Text>
+          </View>
+          <View className="flex-row items-center space-x-3 mr-3">
+            <TouchableOpacity className="bg-white p-2 rounded-full">
+              <Icon.Phone fill={themeColors.bgColor(1)} stroke={themeColors.bgColor(1)} strokeWidth={1} />
+            </TouchableOpacity>
+            <TouchableOpacity
+              onPress={() => navigation.navigate('Home')}
+              className="bg-white p-2 rounded-full">
+              <Icon.X stroke={colors.red} strokeWidth={4} />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </View>
+  )
 }
 
 export default DeliveryScreen
