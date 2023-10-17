047f0a94ad47ff3f19204d3f351ccc43eddd8986
pivosoft <info@pivosoft.com>
2023-10-17 23:56:07 +0300

    added onboarding screens to auth stack

--- app/src/navigation/AppDrawerStack.tsx
+++ app/src/navigation/AppDrawerStack.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { createDrawerNavigator } from '@react-navigation/drawer'
 import { Dimensions } from 'react-native'
 import DrawerScreen from '../screens/DrawerScreen'
@@ -8,39 +8,14 @@ import RestaurantScreen from '../screens/RestaurantScreen'
 import CartScreen from '../screens/CartScreen'
 import PreparingOrderScreen from '../screens/PreparingOrderScreen'
 import DeliveryScreen from '../screens/DeliveryScreen'
-import OnboardingScreen from '../screens/OnboardingScreen'
-import { getItem } from '../async-storage'
 
 const Drawer = createDrawerNavigator()
 
 const AppDrawerStack = () => {
 
-    const [showOnboarding, setShowOnboarding] = useState<boolean | null>(null)
-    const [initialRoute, setInitialRoute] = useState<string | null>(null)
-
-    useEffect(() => {
-        checkIfAlreadyOnboarded()
-    }, [])
-
-    const checkIfAlreadyOnboarded = async () => {
-        let onboarded: any = await getItem('onboarded')
-        console.log(`onboarded value`, onboarded)
-        if (onboarded == 1) {
-            setShowOnboarding(false)
-            setInitialRoute('Home')
-        } else {
-            setShowOnboarding(true)
-            setInitialRoute('Onboarding')
-        }
-    }
-
-    if (showOnboarding == null || initialRoute == null) {
-        return null
-    }
-
     return (
         <Drawer.Navigator
-            initialRouteName={initialRoute}
+            initialRouteName={"Home"}
             screenOptions={{
                 headerShown: false,
                 drawerStyle: {
@@ -49,7 +24,6 @@ const AppDrawerStack = () => {
                 }
             }}
             drawerContent={(props) => <DrawerScreen {...props} />}>
-            <Drawer.Screen name="Onboarding" component={OnboardingScreen} />
             <Drawer.Screen name="Home" component={BottomTabNavigator} />
             <Drawer.Screen name="Help" component={ContactUsStack} />
             <Drawer.Screen name="Restaurant" component={RestaurantScreen} />
--- app/src/navigation/AuthStack.tsx
+++ app/src/navigation/AuthStack.tsx
@@ -1,15 +1,47 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import SigninScreen from "../screens/Authentication/SigninScreen"
 import WelcomeScreen from "../screens/Authentication/WelcomeScreen"
 import SignupScreen from "../screens/Authentication/SignupScreen"
+import OnboardingScreen from "../screens/OnboardingScreen"
+import { getItem } from "../async-storage"
 
 const Stack = createNativeStackNavigator()
 
 const AuthStack = () => {
 
+    const [showOnboarding, setShowOnboarding] = useState<boolean | null>(null)
+    const [initialRoute, setInitialRoute] = useState<string | null>(null)
+
+    useEffect(() => {
+        checkIfAlreadyOnboarded()
+    }, [])
+
+    const checkIfAlreadyOnboarded = async () => {
+        let onboarded: any = await getItem('onboarded')
+        console.log(`onboarded value`, onboarded)
+        if (onboarded == 1) {
+            setShowOnboarding(false)
+            setInitialRoute('Welcome')
+        } else {
+            setShowOnboarding(true)
+            setInitialRoute('Onboarding')
+        }
+    }
+
+    if (showOnboarding == null || initialRoute == null) {
+        return null
+    }
+
     return (
-        <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Navigator initialRouteName={initialRoute}>
+
+            <Stack.Screen
+                name="Onboarding"
+                component={OnboardingScreen}
+                options={{ headerShown: false }}
+
+            />
 
             <Stack.Screen
                 name="Welcome"
